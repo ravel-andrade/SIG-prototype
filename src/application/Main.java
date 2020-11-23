d1a48daca8da21a2b88fd0970964bae1c9270748
Ravel Andrade <ravel.gou@gmail.com>
2020-11-23 16:01:16 -0300

    Implementando menu da main view

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,27 +1,32 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-	try {
-	 FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-	 Parent parent = loader.load();
-	 Scene mainScene = new Scene(parent);
-	 primaryStage.setScene(mainScene);
-	 primaryStage.setTitle("Sample JavaFX application");
-	 primaryStage.show();
-	 } catch (IOException e) {
-	 e.printStackTrace();
-	 }
-	} 
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			ScrollPane scrollPane = loader.load();
+			
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
 
-	
+			Scene mainScene = new Scene(scrollPane);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Sample JavaFX application");
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
