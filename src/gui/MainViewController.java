75d4cbb10e3d4cf78fcac3d29749060637a775e2
Ravel Andrade <ravel.gou@gmail.com>
2020-11-23 17:10:06 -0300

    Implementando tela (Sobre)

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -27,6 +36,8 @@ public class MainViewController implements Initializable {
 	private MenuItem menuItemCustosVariavelDireto;
 	@FXML
 	private MenuItem menuItemCustosVariavelIndireto;
+	@FXML
+	private MenuItem menuItemSobre;
 
 	@FXML
 	public void onMenuItemFuncionarioCadastrarAction() {
@@ -80,9 +91,33 @@ public class MainViewController implements Initializable {
 		System.out.println("onMenuItemCustosVariavelIndiretoAction");
 
 	}
+	
+	@FXML
+	public void onMenuItemSobreAction() {
+		loadView("/gui/Sobre.fxml");
+
+	}
 
 	public void initialize(URL uri, ResourceBundle rb) {
 
+	}
+	
+	private synchronized void loadView(String absoluteName) {
+		try {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+		VBox newVBox = loader.load();
+		Scene mainScene = Main.getMainScene();
+		VBox mainVBox =(VBox)((ScrollPane) mainScene.getRoot()).getContent();
+		
+		Node mainMenu = mainVBox.getChildren().get(0);
+		mainVBox.getChildren().clear();
+		mainVBox.getChildren().add(mainMenu);
+		mainVBox.getChildren().addAll(newVBox.getChildren());
+		}
+		catch(IOException e) {
+			Alerts.showAlert("Erro de IO", "Erro ao carregar a tela", e.getMessage(), AlertType.ERROR);
+		}
+		
 	}
 
 }
